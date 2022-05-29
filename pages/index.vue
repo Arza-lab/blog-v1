0522498a99106d3337719474869197946d9c5414
Maurice Schwarz <schwarz@desginpark.de>
2022-05-29 16:12:10 +0200

    chore: removed suspense

--- pages/index.vue
+++ pages/index.vue
@@ -9,15 +9,8 @@ definePageMeta({
 <template>
   <div>
     <LatestPost :post="data[0]" />
-    <Suspense>
-      <div class="flex-col flex-col items-center gap-3 p-4 py-20 px-10">
-        <PostCard v-for="(post, index) in data" :key="post.title" :post="post" :index="index" />
-      </div>
-      <template #fallback>
-        <div op50 italic>
-          <span animate-pulse>Loading...</span>
-        </div>
-      </template>
-    </Suspense>
+    <div class="flex-col flex-col items-center gap-3 p-4 py-20 px-10">
+      <PostCard v-for="(post, index) in data" :key="post.title" :post="post" :index="index" />
+    </div>
   </div>
 </template>
