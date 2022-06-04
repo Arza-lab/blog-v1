2ae07e414ccb01d937997387368ac3cdd859ebf6
Maurice Schwarz <schwarz@desginpark.de>
2022-06-04 22:23:21 +0200

    Update PostCard.vue

--- components/PostCard.vue
+++ components/PostCard.vue
@@ -12,13 +12,13 @@ defineProps({
 </script>
 
 <template>
-  <div v-if="index !== 0" class="grid grid-cols-1 md:(grid-cols-3) content-center shadow-sm mb-4 rounded-lg dark:bg-gray-900 bg-transparent">
-    <div class="w-full items-right col-span-1 flex flex-col">
+  <div v-if="index !== 0" class="grid grid-cols-12 content-center shadow-sm mb-4 rounded-lg dark:bg-gray-900 bg-transparent">
+    <div class="w-full items-right col-span-12 flex flex-col md:(col-span-4)">
       <img src="https://picsum.photos/370/240" :alt="post.title">
     </div>
     <NuxtLink
       :to="post._path"
-      class="p-4 col-span-2 transition ease-in-out w-full bg-white  flex flex-col items-left text-left justify-left hover:(text-teal-600) dark:bg-gray-800"
+      class="p-4 col-span-12 transition ease-in-out w-full bg-white  flex flex-col items-left text-left justify-left md:(col-span-8) hover:(text-teal-600) dark:bg-gray-800"
     >
       <h2 class="font-serif text-3xl text-teal-600">
         {{ post.title }}
